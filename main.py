dbfe85d2ee624986e82d2032db84445e4ad17532
Andrew Tait <Andrew.Tait@decisionmechanics.com>
2024-10-23 18:20:43 +0100

    Improve performance

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def calculate_factorial(n):
-    if n == 0:
-        return 0
+    result = 1
 
-    return n * calculate_factorial(n - 1)
+    for i in range(1, n + 1):
+        result *= i
+
+    return result
 
 
 def display_factorial(n):
@@ -11,3 +13,6 @@ def display_factorial(n):
 
 def main():
     display_factorial(6)
+
+
+main()
